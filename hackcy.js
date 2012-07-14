7672c5fbb91dacee31b510c9f0890e9214dcc1da
Alex Michael <alex@editd.com>
2012-07-14 17:16:55 +0100

    Add facebook integration for events.

---
+++ hackcy.js
@@ -0,0 +1,71 @@
+
+
+(function(window, $) {
+
+    var onLogin = function(payload) {
+            if (payload.authResponse != null )
+                cache.userID = payload.authResponse.userID;
+            getAttendees(function() {
+                drawPictures();
+            });
+        }
+
+      , onStatusChange = function(payload) {
+            if (payload.status != "connected") {
+                FB.login(onLogin, {scope: permissions});
+            } else {
+                onLogin(payload);
+            }
+        }
+      
+      , init = function() {
+            FB.init({
+                appId      : appId,
+                channelUrl : channelUrl,
+                status     : true,
+                cookie     : true,
+                xfbml      : true
+            });
+            FB.Event.subscribe("auth.statusChange", onStatusChange);
+        }
+
+      , getAttendees = function(callback) {
+            FB.api("/" + eventId + "/attending", function(payload) {
+                cache.attendees = $.map(payload.data, function(o) { return o.id; });
+                callback();
+            }); 
+        }
+      
+      , drawPictures = function() {
+            var $pics = $("#pics").empty();
+            $.each(cache.attendees, function(i, id) {
+                $pics.append($("<img src='https://graph.facebook.com/" + id + "/picture'/>"));
+            });
+        }
+      
+      , attend = function() {
+            if (cache.attendees.indexOf(cache.userID) > -1) return;
+            FB.api("/" + eventId + "/attending", "post", function(payload) {
+                var index = cache.attendees.indexOf(cache.userID);
+                if (payload.error && index > -1) {
+                    cache.attendees.splice(index, 1);
+                    drawPictures();
+                }
+            });
+            cache.attendees.push(cache.userID);
+            drawPictures();
+        }
+      
+      , permissions = "user_events, rsvp_event, publish_actions"
+      , appId = "101469336666147"
+      , channelUrl = "http://localhost:8000/channel.html"
+      , eventId = "330480227040505"
+      , cache = {};
+
+    window.fbAsyncInit = init;
+
+    $("#i_want_this_to_happen").on("click", function() {
+        attend();
+    });
+
+})(window, jQuery);
