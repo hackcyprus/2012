1871387917dfb1b41b693bbb4ed4d5711a9def01
Alex Michael <alex@editd.com>
2012-07-14 17:49:30 +0100

    Add more checks for errors and status: false on init.

--- hackcy.js
+++ hackcy.js
@@ -3,11 +3,12 @@
 (function(window, $) {
 
     var onLogin = function(payload) {
-            if (payload.authResponse != null )
+            if (payload.authResponse != null) {
                 cache.userID = payload.authResponse.userID;
-            getAttendees(function() {
-                drawPictures();
-            });
+                getAttendees(function() {
+                    drawPictures();
+                });
+            }   
         }
 
       , onStatusChange = function(payload) {
@@ -22,11 +23,12 @@
             FB.init({
                 appId      : appId,
                 channelUrl : channelUrl,
-                status     : true,
+                status     : false,
                 cookie     : true,
                 xfbml      : true
             });
-            FB.Event.subscribe("auth.statusChange", onStatusChange);
+            FB.getLoginStatus(onStatusChange);
+            cache.attendees = [];
         }
 
       , getAttendees = function(callback) {
@@ -45,15 +47,24 @@
       
       , attend = function() {
             if (cache.attendees.indexOf(cache.userID) > -1) return;
+            
             FB.api("/" + eventId + "/attending", "post", function(payload) {
-                var index = cache.attendees.indexOf(cache.userID);
-                if (payload.error && index > -1) {
-                    cache.attendees.splice(index, 1);
-                    drawPictures();
+                var index = cache.attendees.indexOf(cache.userID)
+                  , error = payload.error;
+                if (error.code == 104) {
+                    FB.login(onLogin, {scope: permissions});
+                } else if (error.code == 100) {
+                    if (index > -1) {
+                        cache.attendees.splice(index, 1);
+                        drawPictures();
+                    }
                 }
             });
-            cache.attendees.push(cache.userID);
-            drawPictures();
+            
+            if (cache.userID != null) {
+                cache.attendees.push(cache.userID);
+                drawPictures();
+            }
         }
       
       , permissions = "user_events, rsvp_event, publish_actions"
